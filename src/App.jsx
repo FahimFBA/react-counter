2b3d629bffdec9b434c46ff6ff663a231af978cf
FahimFBA <fahimbinamin@gmail.com>
2023-09-29 00:08:33 +0600

    project finished
    create 3 additional functions outside of the return statement

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,24 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
+
+  const setToZero = () => {
+    setCount(0);
+  };
+
   return (
     <div className="App">
       <h1>Counter Project</h1>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Increase Count
-      </button>
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        Decrease Count
-      </button>
-      <button
-        onClick={() => {
-          setCount(0);
-        }}
-      >
-        Set to Zero (0)
-      </button>
+      <button onClick={increase}>Increase Count</button>
+      <button onClick={decrease}>Decrease Count</button>
+      <button onClick={setToZero}>Set to Zero (0)</button>
       <div style={{ fontSize: "25px" }}>{count}</div>
     </div>
   );
